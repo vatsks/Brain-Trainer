17f7395d8c4605430ce2fb9a61b271239076097c
vatsks <iamksvats@gmail.com>
2021-06-27 01:26:47 +0530

    Added timer ,generating random numbers and showing it in a button, updating the answer textview

--- app/src/main/java/com/example/brain_trainer/MainActivity.java
+++ app/src/main/java/com/example/brain_trainer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,8 +15,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView textViewtimer,textViewquestions,textViewansers;Random rand;
-    Button button,button2,button3,button4;int locationofcorrectanswer;
+    TextView textViewtimer,textViewquestions,textViewansers,textView4;Random rand;
+    Button button,button2,button3,button4;int locationofcorrectanswer,score=0,noofques=0;
     ArrayList<Integer> answer=new ArrayList<>();
 
     CountDownTimer countDownTimer=new CountDownTimer(30000,1000) {
@@ -29,11 +30,43 @@ public class MainActivity extends AppCompatActivity {
             textViewtimer.setText("00s");
         }
     };
-        public void click(View view){
-            int k=Integer.parseInt(button.getText().toString());
-            if(k==locationofcorrectanswer){
-                textViewquestions.setText("correct");
+        public void setquestion(){
+
+            rand = new Random();
+            int a= rand.nextInt(20);
+            int b= rand.nextInt(61);
+            textViewquestions.setText(Integer.toString(a)+"+"+Integer.toString(b));
+            locationofcorrectanswer = rand.nextInt(4);
+            for(int i=0;i<4;i++){
+                if (i==locationofcorrectanswer){
+                    answer.add(a+b);
+                }
+                else {
+                    int wringanswer=rand.nextInt(80);
+                    while(wringanswer==a+b){
+                        answer.add(rand.nextInt(20)+rand.nextInt(60));}
+
+                    answer.add(wringanswer);
+                }
+
             }
+            button.setText((Integer.toString(answer.get(0))));
+            button2.setText((Integer.toString(answer.get(1))));
+            button3.setText((Integer.toString(answer.get(2))));
+            button4.setText((Integer.toString(answer.get(3))));
+        }
+
+        public void click(View view){
+           if (Integer.toString(locationofcorrectanswer).equals(view.getTag().toString())){
+               textView4.setText("Correct");score++;
+
+           }else {
+               textView4.setText("not correct");
+           }
+            noofques++;
+           textViewansers.setText(Integer.toString(score)+"/"+Integer.toString(noofques));
+           setquestion();
+
 
         }
 
@@ -47,29 +80,11 @@ public class MainActivity extends AppCompatActivity {
         button3=findViewById(R.id.button3);
         button4=findViewById(R.id.button4);
         textViewquestions=findViewById(R.id.textViewquestions);
+        textViewansers=findViewById(R.id.textViewanswers);
+        textView4=findViewById(R.id.textView4);
         countDownTimer.start();
-        rand = new Random();
-        int a= rand.nextInt(20);
-        int b= rand.nextInt(61);
-        textViewquestions.setText(Integer.toString(a)+"+"+Integer.toString(b));
-        locationofcorrectanswer = rand.nextInt(4);
-        for(int i=0;i<4;i++){
-            if (i==locationofcorrectanswer){
-                answer.add(a+b);
-            }
-            else {
-                int wringanswer=rand.nextInt(80);
-                while(wringanswer==a+b){
-                answer.add(rand.nextInt(20)+rand.nextInt(60));}
+        setquestion();
 
-            answer.add(wringanswer);
-        }
-
-    }
-        button.setText((Integer.toString(answer.get(0))));
-        button2.setText((Integer.toString(answer.get(1))));
-        button3.setText((Integer.toString(answer.get(2))));
-        button4.setText((Integer.toString(answer.get(3))));
 
 
     }
